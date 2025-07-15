be0292064d6827550a72d9849eb8066f82f22b26
chadmortensen <chadmor@gmail.com>
2025-07-15 16:29:11 -0700

    img

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -9,7 +9,7 @@ const CaseStudies = () => {
     company: "Walmart", 
     duration: "1 quarter",
     teamSize: "6 people",
-    image: "./src/img/baby-registry-walmart.png?auto=format&fit=crop&w=800&q=80",
+    image: "/src/img/baby-registry-walmart.png?auto=format&fit=crop&w=800&q=80",
     challenge: "In 1 quarter; design, develop and launch an improved baby registry experience addressing shortcomings of the previous registry tool.",
     goals: ["Increase quality registry creations (creations that lead to a first curation action)", "Increase curation by 10%", "Increase sharing by 20%", "Increase purchase conversion by 25%", "Helping new parents with this major moment in life"],
     route: "/case-study-1"
