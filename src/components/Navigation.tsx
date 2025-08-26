245554f394a47e78eee0ffb762b5e9f0ab3127c0
chadmortensen <chadmor@gmail.com>
2025-08-26 09:00:48 -0700

    Moved view all case study items to end

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -74,12 +74,6 @@ const Navigation = () => {
                     <span className="absolute -bottom-1 left-0 w-0 h-px bg-accent-blue group-hover:w-full transition-all duration-300"></span>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent className="bg-surface-primary border border-swiss-light shadow-lg">
-                    <DropdownMenuItem
-                      onClick={() => scrollToSection(item.href)}
-                      className="text-text-secondary hover:text-text-primary hover:bg-surface-secondary cursor-pointer"
-                    >
-                      View All Case Studies
-                    </DropdownMenuItem>
                     {caseStudies.map((study) => (
                       <DropdownMenuItem
                         key={study.route}
@@ -89,6 +83,12 @@ const Navigation = () => {
                         {study.title}
                       </DropdownMenuItem>
                     ))}
+                    <DropdownMenuItem
+                      onClick={() => scrollToSection(item.href)}
+                      className="text-text-secondary hover:text-text-primary hover:bg-surface-secondary cursor-pointer"
+                    >
+                      View All Case Studies
+                    </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
               ) : (
