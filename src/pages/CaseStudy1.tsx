ec6d91126acbadec72fa76e77f8ed0a0b3299ae8
chadmortensen <chadmor@gmail.com>
2025-08-26 09:07:34 -0700

    Updated case studie titles

--- src/pages/CaseStudy1.tsx
+++ src/pages/CaseStudy1.tsx
@@ -30,8 +30,8 @@ const CaseStudy1 = () => {
     subheader?: string;
     modules: Module[];
   }>>([]);
-  const [title, setTitle] = useState('A rapid revamp to the Walmart registry');
-  const [subtitle, setSubtitle] = useState('Building a customer-centered registry experience under tight constraints');
+  const [title, setTitle] = useState('Walmart eCommerce');
+  const [subtitle, setSubtitle] = useState('Building a customer-centered registry experience under tight constraints (2019)');
 
   const sensors = useSensors(
     useSensor(PointerSensor),
