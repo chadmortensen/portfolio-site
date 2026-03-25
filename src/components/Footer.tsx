3b480772568e1daf97ec4c466def6bcf1d5bf3fa
chadmortensen <chadmor@gmail.com>
2026-03-25 12:21:38 -0700

    Update footer copyright year dynamically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="py-12 bg-surface-primary border-t border-swiss-light">
       <div className="swiss-grid">
         <div className="col-span-12 text-center">
           <p className="text-caption text-text-tertiary">
-            © 2024 Chad Mortensen. All rights reserved.
+            © {currentYear} Chad Mortensen. All rights reserved.
           </p>
         </div>
       </div>
